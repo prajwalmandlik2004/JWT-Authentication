88dc2d077bdc856685844d20f9ec2ae4864d0c86
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2024-02-04 19:53:16 +0530

    changes

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/registration')
+mongoose.connect(process.env.CONNECTION_MONGO)
     .then(() => console.log("Connect Successfully !!"))
     .catch((error) => console.log(error));
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const Register = require('../models/registers');
 const auth = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
-        const verifyUser = jwt.verify(token, 'mynameisprajwalmandlikbakenddeveloper');
+        const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
         console.log(verifyUser);
 
         const user = await Register.findOne({ _id: verifyUser._id });
